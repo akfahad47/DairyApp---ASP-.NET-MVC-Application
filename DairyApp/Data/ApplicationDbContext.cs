4ca9c55f1189b639e872294f9a8df21b1edfc213
akfahad47 <hello.akfahad@gmail.com>
2025-05-02 00:41:04 -0400

    Add project files.

---
+++ DairyApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DairyApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DairyApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+    }
+}
