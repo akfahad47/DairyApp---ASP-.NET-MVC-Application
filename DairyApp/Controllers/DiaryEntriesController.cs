88481ca3bd09e29904787287637b7ac1facf8c73
akfahad47 <hello.akfahad@gmail.com>
2025-05-05 12:52:34 -0400

    Added server side validation and mostly covered all the CRUD operations.

--- DairyApp/Controllers/DiaryEntriesController.cs
+++ DairyApp/Controllers/DiaryEntriesController.cs
@@ -12,21 +12,97 @@ namespace DairyApp.Controllers
         {
             _db = db;
         }
+
         public IActionResult Index()
         {
             List<DiaryEntry> objDiaryEntryList = _db.DiaryEntries.ToList();
             return View(objDiaryEntryList);
         }
+
         public IActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Create(DiaryEntry obj)
         {
-            _db.DiaryEntries.Add(obj);  // Adds the new diary entry to the database
-            _db.SaveChanges(); // Saves the changes to the database
-            return RedirectToAction("Index", "Home");
+            if(obj != null && obj.Title.Length < 3)
+            {
+                ModelState.AddModelError("Title", "Title too short");
+            }
+
+            if (ModelState.IsValid) 
+            {
+                _db.DiaryEntries.Add(obj);  // Adds the new diary entry to the database
+                _db.SaveChanges(); // Saves the changes to the database
+                return RedirectToAction("Index", "Home");
+            }
+            return View(obj);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+
+            if (diaryEntry == null) 
+            {
+                return NotFound();
+            }
+
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(DiaryEntry obj)
+        {
+            if (obj != null && obj.Title.Length < 3)
+            {
+                ModelState.AddModelError("Title", "Title too short");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.DiaryEntries.Update(obj);  // Edit the new diary entry to the database
+                _db.SaveChanges(); // Saves the changes to the database
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(DiaryEntry obj)
+        {
+            
+            _db.DiaryEntries.Remove(obj);  // Remove the new diary entry to the database
+            _db.SaveChanges(); // Saves the changes to the database
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- DairyApp/Models/DiaryEntry.cs
+++ DairyApp/Models/DiaryEntry.cs
@@ -7,8 +7,8 @@ namespace DairyApp.Models
         //[Key]
         public int Id { get; set; }
         [Required(ErrorMessage = "Please enter a title!")]
-        [StringLength(100, MinimumLength = 3, 
-            ErrorMessage = "Title must be between 3 and 100 Characters!")]
+        //[StringLength(100, MinimumLength = 3, 
+        //    ErrorMessage = "Title must be between 3 and 100 Characters!")]
         public string Title { get; set; } = string.Empty;
         [Required]
         public string Content { get; set; } = string.Empty;
